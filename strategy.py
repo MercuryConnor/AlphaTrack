238f72b011725117317e6d75a27af09e82929a5d
MercuryConnor <119674412+MercuryConnor@users.noreply.github.com>
2025-07-07 13:38:11 +0530

    bugfix

--- strategy.py
+++ strategy.py
@@ -30,7 +30,7 @@ def sma_crossover_signals(data: pd.DataFrame, short_window: int, long_window: in
     signals['signal'] = 0
     signals['short_ma'] = data[f'SMA_{short_window}']
     signals['long_ma'] = data[f'SMA_{long_window}']
-    signals['signal'][short_window:] = (
+    signals.loc[signals.index[short_window:], 'signal'] = (
         signals['short_ma'][short_window:] > signals['long_ma'][short_window:]
     ).astype(int)
     signals['positions'] = signals['signal'].diff()
